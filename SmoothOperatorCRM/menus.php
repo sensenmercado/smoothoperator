bce3c13143bed7f106e26f4eb8f1a17b3518c32f
matt <matt@venturevoip.com>
2009-10-06 05:36:59 +0000

    Change menus to include child_of field and add the auto table creation code

--- SmoothOperatorCRM/menus.php
+++ SmoothOperatorCRM/menus.php
@@ -44,7 +44,7 @@ $(document).ready(function() {
     <ul id="test-list" class="xxxx">
         <?
         /* TODO: USE ALL LANGUAGES */
-        $result = mysqli_query($connection, "SELECT id, menu_order, menu_text, link FROM menu_items WHERE language = 'en' AND visible = 1 AND is_setting = 0 ORDER BY menu_order") or die(mysqli_error($connection));
+        $result = mysqli_query($connection, "SELECT id, menu_order, menu_text, link FROM menu_items WHERE language = 'en' AND visible = 1 AND child_of = -1 ORDER BY menu_order") or die(mysqli_error($connection));
         if (mysqli_num_rows($result) > 0) {
             while ($row = mysqli_fetch_assoc($result)) {
                 echo '<li class="xxxx" id="listItem_'.$row['id'].'" align="left">';
