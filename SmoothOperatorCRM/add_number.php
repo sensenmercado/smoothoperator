5c9b63e7a92efc470c60d7c1b51be3f30cc3365e
matt@venturevoip.com <matt@venturevoip.com@08a2b738-b151-11de-9d1a-45ab25924541>
2009-10-07 00:04:32 +0000

    Add a page to add a new customer record (and add the link to the database creation script)

---
+++ SmoothOperatorCRM/add_number.php
@@ -0,0 +1,12 @@
+<?php
+require "header.php";
+?>
+<div class="xxxx"  style="background: #cdf;width: 300px;margin-top: 30px;padding:10px;">
+<form action="get_customer.php" method="get">
+Phone Number: <input type="text" name="phone_number">
+<input type="submit" value="Create Record">
+</form>
+</div>
+<?
+require "footer.php";
+?>
