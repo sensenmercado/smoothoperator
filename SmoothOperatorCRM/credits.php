96e6832074a0c0224a2510676ebcb04302351c37
matt <matt@venturevoip.com>
2009-10-09 23:05:26 +0000

    Reasonably large commit - the module installation now supports base64 encoded .tar.gz archives of packages to install, decompresses them, adds menu items, runs a patch (using a unified diff encoded with base64) to change any files to use the SmoothOperator config system and installs the files.  It also saves the file names to the DB so that they can be removed on uninstallation.  Config options are not currently removed.
    So, for example, with the Areski CDR Stats package it does this:
    
    1. Download XML File
    2. Parse it for archives
    3. Parse it for patches
    4. Parse it for menus
    5. Parse it for config options
    
    Next it installs the above.
    
    Menus appear as menus in the system (and can be moved around etc like normal menus), config options get added to the sourcecode via the patch - i.e. in CDR stats there is a file lib/defines.php where you need to specify DB connection details hard coded.  It patches this so that the config options come from $config_values (stored in $_SESSION), and the options appear in the config section of the main site.  So for example it provides $config_values['cdr_host'].
    
    archive.php allows us to deal with .tar.gz files and extract them without using the base operating system
    
    phppatcher.php allows us to patch the directory structure of the installed files.
    
    At the moment, the cdr directory comes in SVN and this is the subdirectory that is used to store the files - maybe it would be better to use a modules/ directory.

---
+++ SmoothOperatorCRM/credits.php
@@ -0,0 +1,5 @@
+<?php
+?>
+PHP unified diff patcher: http://sourceforge.net/projects/phppatcher/
+TAR/GZIP/BZIP2/ZIP ARCHIVE CLASSES 2.1: Devin Doucette Email: darksnoopy@shaw.ca
+
--- SmoothOperatorCRM/view_modules.php
+++ SmoothOperatorCRM/view_modules.php
@@ -12,6 +12,116 @@ if (isset($_GET[filename])) {
     $xml = simplexml_load_file("./modules/".$_GET[filename]);
     $module_type = $xml->type;
 
+    $result = mysqli_query($connection, "SELECT id FROM modules WHERE name = ".sanitize($_GET[filename]));
+    if (mysqli_num_rows($result) > 0) {
+        /* Module entry exists */
+        $row = mysqli_fetch_assoc($result);
+        $module_id = $row['id'];
+    } else {
+        $result = mysqli_query($connection, "INSERT INTO modules (name) VALUES (".sanitize($_GET[filename]).")");
+        $module_id = mysqli_insert_id();
+    }
+    if ($_GET[action] == "install") {
+        unset($file_contents);
+        if (isset($xml->archive)) {
+            $file_contents = base64_decode($xml->archive->contents);
+            $filename = dirname(__FILE__)."/".$xml->archive->filename;
+            $have_file = false;
+            if (file_exists($filename)) {
+                /* File already Exists */
+                $have_file = true;
+            } else {
+                $fp = fopen($filename, w);
+                if (!$fp) {
+                    /* File creation failed */
+                    echo "File creation of $filename failed";
+                    $have_file = false;
+                } else {
+                    /* File creation succeeded */
+                    if (fwrite($fp, $file_contents)) {
+                        $have_file = true;
+                    } else {
+                        $have_file = false;
+                    }
+                    fclose($fp);
+                }
+            }
+            if ($have_file) {
+                require "archive.php";
+                $test = new gzip_file($filename);
+                $test->set_options(array('overwrite' => 1, 'inmemory' => 0));
+                $test->extract_files();
+
+                /* Doesn't create directories when running as inmemory and */
+                /* doesn't create the list of files running without...     */
+                $test->set_options(array('overwrite' => 1, 'inmemory' => 1));
+                $test->extract_files();
+                $test->make_list();
+                foreach ($test->files as $file) {
+                    $sql = "INSERT INTO module_files (module_id, file_name) VALUES ($module_id, ".sanitize($file['name']).")";
+                    $result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
+                }
+                
+
+                if (isset($xml->patch->base)) {
+                    require "phppatcher.php";
+                    $patch = new PhpPatcher($xml->patch->base);
+                    $patch->Merge(base64_decode($xml->patch->diff));
+                    //$patch->msg;
+                    $patch->ApplyPatch();
+                }
+            }
+            if (isset($xml->config_options)) {
+                foreach ($xml->config_options->option as $option) {
+                    unset($sql);
+                    /* sanitize needs ""+ because it automatically detects object type */
+                    $sql1 = "INSERT INTO config (parameter, value) VALUES (".sanitize("".$option->name, true).", ".sanitize("".$option->value, true).")";
+                    $sql2 = "INSERT IGNORE INTO static_text (parameter, language, description) VALUES (".sanitize("".$option->name).",'en_gb', ".sanitize("".$option->text).")";
+                    $result = mysqli_query($connection, $sql1);
+                    $result = mysqli_query($connection, $sql2);
+
+                }
+            }
+            //exit(0);
+        }
+    } else if ($_GET[action] == "uninstall") {
+        $sql = "SELECT id FROM modules WHERE name = ".sanitize($_GET['filename']);
+        $result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
+        if (mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            $module_id = $row['id'];
+            $result = mysqli_query($connection, "SELECT file_name FROM module_files WHERE module_id = $module_id order by LENGTH(file_name) desc");
+            if (mysqli_num_rows($result) > 0) {
+                while($row = mysqli_fetch_assoc($result)) {
+                    $file_name = $row['file_name'];
+                    //echo $file_name."<br />";
+                    if (substr($file_name, strlen($file_name) - 1) == "/") {
+                        rmdir($file_name);
+                    } else {
+                        unlink($file_name);
+                    }
+                    $sql = "DELETE FROM module_files WHERE module_id = $module_id AND file_name = ".sanitize($file_name);
+                    $result_delete = mysqli_query($connection, $sql);
+                }
+            }
+        }
+
+
+        //exit(0);
+
+        unset($filename);
+        if (isset($xml->archive)) {
+            $filename = dirname(__FILE__)."/".$xml->archive->filename;
+            if (strlen($filename) > 3) {
+                /* Just in case the impossible happens don't  */
+                /* unlink the root - should be impossible but */
+                /* better safe than sorry                     */
+                unlink($filename);
+            }
+        }
+    }
+
+
     if ($_GET[action] == "install"||$_GET[action] == "uninstall") {
         $menus = $xml->menu;
 
@@ -25,7 +135,7 @@ if (isset($_GET[filename])) {
                     //echo "Language: $key String: $value Link $link Security_Level: $security_level Use Iframe: $use_iframe<br />";
                     $menu_text = sanitize("".$value);
                     $language = sanitize("".$key);
-                    if ($_GET[action] == "install") {
+                    if ($_GET['action'] == "install") {
                         /* ************************************************** */
                         /* INSTALL A MENU ITEM                                */
                         /* ************************************************** */
@@ -33,12 +143,14 @@ if (isset($_GET[filename])) {
                         $sql = "INSERT INTO menu_items (menu_text, language, security_level, link, use_iframe) VALUES (";
                         $sql .=$menu_text.",".$language.",".$security_level.",".$link.",".$use_iframe;
                         $sql.=")";
-
+                        //echo $sql;
+                        $result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
                         /* ************************************************** */
                         /* CREATE A PAGE                                      */
                         /* ************************************************** */
 
-                        unset($file_contents);
+                        
+
                         if (isset($menu_item->page)) {
                             $file_contents = base64_decode($menu_item->page->content);
                             $filename = dirname(__FILE__)."/".$menu_item->link;
@@ -73,9 +185,9 @@ if (isset($_GET[filename])) {
                         /* REMOVE A MENU ITEM                                 */
                         /* ************************************************** */
 
-                        $sql = "DELETE FROM menu_items WHERE menu_text=$menu_text AND language=$language AND link=$link";
 
 
+                        unset($filename);
                         if (isset($menu_item->page)) {
                             $filename = dirname(__FILE__)."/".$menu_item->link;
                             if (strlen($filename) > 3) {
@@ -85,11 +197,12 @@ if (isset($_GET[filename])) {
                                 unlink($filename);
                             }
                         }
+                        //echo $sql."<br />";
+                        $sql = "DELETE FROM menu_items WHERE menu_text=$menu_text AND language=$language AND link=$link";
+                        $result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
 
                     }
-                    //echo $sql."<br />";
-
-                    $result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
+                    
                 }
             }
         }
