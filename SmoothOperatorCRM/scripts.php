91a182d73a948c528434c4970752fae95d97a2c8
matt@venturevoip.com <matt@venturevoip.com@08a2b738-b151-11de-9d1a-45ab25924541>
2009-10-21 05:50:16 +0000

    Commit comments on the structure of how I see scripts working - should cover any need as you can have however many questions you like, and can have text boxes, yes/no, or a combo box (box with multiple choices in it).   Can just change the comments if we need to add extra types in the future - if anyone has any comments on things that should be available in a script, just post.

---
+++ SmoothOperatorCRM/scripts.php
@@ -0,0 +1,45 @@
+<?php
+/* 
+ * The idea here is to load and process customer records using scripts. This
+ * differs from normal displaying of customer records in much the same way as a
+ * mail merge works.
+ *
+ * A script is a series of statements or questions that the call centre staff
+ * member will read as they respond to a customer.  This way you can have staff
+ * that are working simultaneously on multiple projects.  It can be useful for
+ * outbound marketing as well as inbound calls as it allows for an easy way to
+ * store information.
+ *
+ * A script is based on the following MySQL structure:
+ *
+ * id - autoincrement index (automatically created)
+ * name - name of the script
+ * description - description of the script
+ * owner - the id of the user who created this script
+ * lastupdated - automatically updated timestamp for last update
+ * groupid - the id of a group of people allowed access to this script
+ *
+ * You then have script entries which are stored in the script_entries table:
+ *
+ * id - autoincrement index (automatically created)
+ * script_id - id of the script that this refers to
+ * type - the type of script entry i.e.:
+ *       -1 - end of section/page
+ *        0 - statement followed by text field
+ *        1 - statement followed by a yes/no field
+ *        2 - statement followed by a combo box field
+ *        3 - statement followed by nothing
+ * statement - the text to display
+ * order - the position of the entry in the script as a whole
+ *
+ * If you are using a combo box field, the choices are supplied from the
+ * combo_choices table.  This table has the following structure:
+ *
+ * id - autoincrement index (automatically created)
+ * script_id - id of the script that this refers to
+ * text - the text of the choice (i.e. 'apple' or 'pear')
+ * value - the value to be used when saving to database
+ *
+*/
+
+?>
