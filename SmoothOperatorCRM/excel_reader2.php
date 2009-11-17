2f95b09bd0df5e5959354d093d8250ae485b1559
matt@venturevoip.com <matt@venturevoip.com@08a2b738-b151-11de-9d1a-45ab25924541>
2009-11-17 09:07:29 +0000

    A couple of changes

--- SmoothOperatorCRM/excel_reader2.php
+++ SmoothOperatorCRM/excel_reader2.php
@@ -31,8 +31,16 @@
  *
  * PHP versions 4 and 5
  *
- * LICENSE: This source file is subject to version 3.0 of the PHP license
- * that is available through the world-wide-web at the following URI:
+ * LICENSE: This source file is dual licensed under the following:
+ *
+ * Version 2.0 of the GPL license that is available through the world-wide-web
+ * at the following URI:
+ * 
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * Version 3.0 of the PHP license that is available through the world-wide-web
+ * at the following URI:
+ * 
  * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
  * the PHP License and are unable to obtain it through the web, please
  * send a note to license@php.net so we can mail you a copy immediately.
