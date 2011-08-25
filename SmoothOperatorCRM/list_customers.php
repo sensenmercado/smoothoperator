c22aebb09ff29c8fd1a6bbad9cd58ae80bf3d164
matt <matt@venturevoip.com>
2011-08-25 01:55:14 +0000

    Fix formatting and add case for missing table

--- SmoothOperatorCRM/list_customers.php
+++ SmoothOperatorCRM/list_customers.php
@@ -6,26 +6,30 @@ if (!isset($_GET['list'])) {
     //redirect("manage_lists.php");
     ?>
     <div class="thick_700_box" >
-<?
-$result  = mysqli_query($connection, "SELECT * FROM lists");
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $result2 = mysqli_query($connection, "SELECT Count(*) as total FROM customers WHERE list_id = ".$row['id']);
-        if (mysqli_num_rows($result2) > 0) {
-            $row2 = mysqli_fetch_assoc($result2);
-            $count = $row2['total'];
+    <?
+    $result  = mysqli_query($connection, "SELECT * FROM lists");
+    if ($result) {
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $result2 = mysqli_query($connection, "SELECT Count(*) as total FROM customers WHERE list_id = ".$row['id']);
+                if (mysqli_num_rows($result2) > 0) {
+                    $row2 = mysqli_fetch_assoc($result2);
+                    $count = $row2['total'];
+                }
+                //print_pre($row);
+                //print_pre($row2);
+                echo '<a href="list_customers.php?list='.$row['id'].'">'.$row['name'].' ('.number_format($count).' Numbers)</a><br />';
+                //echo "<hr />";
+            }
+        } else {
+            echo 'You have no lists of numbers. Click here to create one';
+            
         }
-        //print_pre($row);
-        //print_pre($row2);
-        echo '<a href="list_customers.php?list='.$row['id'].'">'.$row['name'].' ('.number_format($count).' Numbers)</a><br />';
-        //echo "<hr />";
+    } else {
+        echo 'You have no lists of numbers. Click here to create one';        
     }
-} else {
-    echo 'You have no lists of numbers. Click here to create one';
-
-}
-?>
-</div>
+    ?>
+    </div>
     <?
     require "footer.php";
     exit(0);
@@ -55,7 +59,7 @@ while ($row = mysqli_fetch_assoc($result)) {
         }
         echo "</tr>";
     }
-
+    
     echo "<tr onmouseover=\"this.style.background='#bcf ';this.style.cursor='pointer'\" onmouseout=\"this.style.background='#eee';\" onclick=\"window.location.href='get_customer.php?from=list&phone_number=".$row['phone']."';\">";
     foreach ($row as $field=>$value) {
         if (in_array($field, $printable)) {
