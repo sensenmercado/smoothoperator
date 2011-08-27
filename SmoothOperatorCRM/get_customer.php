8220653a5985dfa3a601006abc3a20eee0bb3711
matt <matt@venturevoip.com>
2011-08-27 02:35:03 +0000

    Couple more strip slashes entries

--- SmoothOperatorCRM/get_customer.php
+++ SmoothOperatorCRM/get_customer.php
@@ -67,21 +67,28 @@
         echo '<table class="sample">';
         foreach ($row as $field=>$value) {
             if (in_array($field, $fields_to_hide)) {
-                echo '<input type="hidden" name="'.$field.'" value="'.$value.'">';
+                echo '<input type="hidden" name="'.$field.'" value="'.stripslashes($value).'">';
             } else if (in_array($field, $textarea_fields)) {
                 echo '<tr><th colspan="2">'.clean_field_name($field).'</th></tr>';
-                echo '<tr><td colspan="2"><textarea cols="60" rows="10" name="'.$field.'">'.$value.'</textarea></td></tr>';
+                echo '<tr><td colspan="2"><textarea cols="60" rows="10" name="'.$field.'">'.stripslashes($value).'</textarea></td></tr>';
             } else {
-                echo '<tr><th>'.clean_field_name($field).'</th><td><input type="text" name="'.$field.'" style="width: 400px" value="'.$value.'"></td></tr>';
+                echo '<tr><th>'.clean_field_name($field).'</th><td><input type="text" name="'.$field.'" style="width: 400px" value="'.stripslashes($value).'"></td></tr>';
             }
         }
         echo '<tr><td colspan="2"><input type="submit" value="save changes"></td></tr>';
         echo '</form>';
         echo "</table>";
     }
+    
+    
+    /* Disposition */
     echo '<div class="thin_700px_box">';
     echo "Disposition:";
-    echo "</div>";
+    echo '</div>';
+    
+    
+    
+    
     $phone_number = clean_number($_GET[phone_number]);
     $result = mysqli_query($connection, "SELECT * FROM SmoothOperator.customers WHERE cleaned_number = '$phone_number'");
     if (mysqli_num_rows($result) > 0) {
