b03489a389a8d1ec53df218a9d7d0b84395fb97a
matt <matt@venturevoip.com>
2011-09-06 08:14:43 +0000

    Move the status bar to the bottom so the menu options don't move

--- SmoothOperatorCRM/check_job.php
+++ SmoothOperatorCRM/check_job.php
@@ -4,7 +4,9 @@ require "config/db_config.php";
 
 /* Include sanitize functions */
 require "functions/sanitize.php";
-
+if (!isset($_GET['user_id'])) {
+    $_GET['user_id'] = $_SESSION['user_id'];
+}
 $sql = "SELECT * FROM job_members, jobs WHERE job_members.job_id = jobs.id and job_members.user_id = ".sanitize($_GET['user_id']);
 $result = mysqli_query($connection, $sql);
 if ($result && mysqli_num_rows($result) > 0) {
