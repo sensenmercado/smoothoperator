49a80934f5cf71bc9ac0df8ab3c98974b99d73c2
matt <matt@venturevoip.com>
2011-09-22 23:54:17 +0000

    Add a skeleton file to start adding reports

--- SmoothOperatorCRM/reports.php
+++ SmoothOperatorCRM/reports.php
@@ -1,8 +1,46 @@
 <?
 require "header.php";
+if (isset($_GET['save'])) {
+    $sql = "INSERT INTO reports (name, description) VALUES (".sanitize($_POST['name']).", ".sanitize($_POST['description']).")";
+    mysqli_query($connection, $sql);
+    redirect("reports.php",0);
+    require "footer.php";
+    exit(0);
+}
+if (isset($_GET['add'])) {
+    ?>
+    <br />
+    <form action="reports.php?save=1" method="post">
+    <table class="sample">
+    <tr>
+    <th>Report Name</th>
+    <td><input type="text" name="name"></td>
+    </tr>
+    <tr>
+    <th colspan="2">Report Description</th></tr>
+    <tr>
+    <td colspan="2"><textarea name="description"></textarea></td>
+    </tr>
+    <tr>
+    <td colspan="2"><input type="submit" value="Add Report"></td>
+    </tr>
+    </table>
+    </form>
+    <?
+    require "footer.php";
+    exit(0);
+}
+if (!isset($_GET['report_id'])) {
+    $result = mysqli_query($connection, "SELECT * FROM reports");
+    if (mysqli_num_rows($result) == 0) {
+        echo '<br />There are currently no jobs defined - please <a href="reports.php?add=1">add one by clicking here.</a>';
+    } else {
+        while ($row = mysqli_fetch_assoc($result)) {
+            print_pre($row);
+        }
+    }
+}
 ?>
-<br />
-<img src="drawpie.php">
 <?
 require "footer.php";
 ?>
