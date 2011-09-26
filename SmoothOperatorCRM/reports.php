dfc1f5d991096e7ff4c686465d99c75b77edc2bc
matt@venturevoip.com <matt@venturevoip.com@08a2b738-b151-11de-9d1a-45ab25924541>
2011-09-26 00:54:41 +0000

    Provide delete functionality

--- SmoothOperatorCRM/reports.php
+++ SmoothOperatorCRM/reports.php
@@ -1,6 +1,45 @@
 <?
 $rounded[] = 'div.thin_700px_box';
 require "header.php";
+if (isset($_GET['delete_sure'])) {
+    ?><div class="thin_700px_box"><?
+    $id = sanitize($_GET['delete_sure']);
+    //TODO: Make this delete script entries
+    $result = mysqli_query($connection, "DELETE FROM reports WHERE id = $id");
+    if (!$result) {
+        $messages[] = "There was a problem deleting this report: ".mysqli_error();
+        $_SESSION['messages'] = $messages;
+    }
+    draw_progress("Please wait we are saving your changes...");
+    redirect("reports.php", 0);
+    ?></div><?
+    require "footer.php";
+    exit(0);
+}
+if (isset($_GET['delete'])) {
+    ?><div class="thin_700px_box"><?
+    ?>
+    Are you sure you would like to delete this report?<br />
+    <br />
+    <?
+    $id = sanitize($_GET['delete']);
+    $result = mysqli_query($connection, "SELECT * FROM reports WHERE id = $id");
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        echo "<b>Name: </b>".$row['name']."<br />";
+        echo "<b>Description: </b>".$row['description']."<br />";
+    }
+    ?>
+    <br />
+    <a href="reports.php?delete_sure=<?=$_GET['delete']?>">Yes, delete it</a><br />
+    <a href="reports.php">No, do not delete it</a><br />
+        </div><?
+        require "footer.php";
+    exit(0);
+}
+if (isset($_GET['edit'])) {
+    
+}
 if (isset($_GET['save'])) {
     $sql = "INSERT INTO reports (name, description) VALUES (".sanitize($_POST['name']).", ".sanitize($_POST['description']).")";
     mysqli_query($connection, $sql);
