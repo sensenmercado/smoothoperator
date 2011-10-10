7582e7b9b6c1d707222e282425504b7b383f2044
matt@venturevoip.com <matt@venturevoip.com@08a2b738-b151-11de-9d1a-45ab25924541>
2011-10-10 00:04:50 +0000

    Start integrating dialer

---
+++ SmoothOperatorCRM/dialer.php
@@ -0,0 +1,11 @@
+<?
+require "header.php";
+?>
+<div class="thin_700px_box">
+<br />
+From here you can run lists of numbers through the SmoothTorque dialer.<br />
+<br />
+</div>
+<?
+require "footer.php";
+?>
