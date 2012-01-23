6225956028cc99bae8ba4234c36eda340276065d
matt@venturevoip.com <matt@venturevoip.com@08a2b738-b151-11de-9d1a-45ab25924541>
2012-01-23 01:57:16 +0000

    Change slightly odd code ordering

--- SmoothOperatorCRM/jobs.php
+++ SmoothOperatorCRM/jobs.php
@@ -84,14 +84,14 @@ if (isset($_GET['save_members'])) {
             $sqls[] = "INSERT INTO SineDialer.queue_member_table (queue_name, membername, interface) VALUES ('so_crm_".sanitize($_GET['save_members'],false)."', ".sanitize($username).",".sanitize("Agent/".$agent_num).")";
             
             
-
+            
         }        
         /* Connect to SmoothTorque MySQL Database */
         $link = mysql_connect($config_values['smoothtorque_db_host'], $config_values['smoothtorque_db_user'], $config_values['smoothtorque_db_pass']) or die(mysql_error());
         foreach ($sqls as $sql) {
             mysql_query($sql) or die(mysql_error());
         }
-                
+        
         
     }    
     //echo $sql."\n";
@@ -123,7 +123,7 @@ if (isset($_GET['save'])) {
         $result = mysql_query($sql);
         
     }
-
+    
     
     /* Redirect to edit the new job */
     redirect("jobs.php?job_id=".$job,0);
@@ -185,206 +185,212 @@ if (isset($_GET['add'])) {
     exit(0);
 }
 
-if (!isset($_GET['job_id'])) {
-    $rounded[] = "div.box";
+
+
+
+if (isset($_GET['job_id'])) {
+    $rounded[] = 'div.panel_l';
+    $rounded[] = 'div.panel_r';
+    $rounded[] = 'div.panel_t';
+    $rounded[] = 'div.panel_b';
     require "header.php";
-    ?>
-    <div class="box">
-    <?
-    $result = mysqli_query($connection, "SELECT id, name, description FROM jobs") or die(mysqli_error($connection));;
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            ?>
-            <a href="jobs.php?job_id=<?=$row['id']?>"><img src="images/pencil.png" border="0">&nbsp;<?=$row['name']?></a><a href="jobs.php?delete=<?=$row['id']?>"><img src="images/delete.png" border="0"></a><br />
-            <?
-            
-        }
-    }
-    ?>
-    </div>
     
+    ?>
+    <script type='text/javascript' src='js/multiselect.js'></script>
     <?
-    require "footer.php";
-    exit(0);
-}
-$rounded[] = 'div.panel_l';
-$rounded[] = 'div.panel_r';
-$rounded[] = 'div.panel_t';
-$rounded[] = 'div.panel_b';
-require "header.php";
-
-?>
-<script type='text/javascript' src='js/multiselect.js'></script>
-<?
-
-$result = mysqli_query($connection, "SELECT id FROM users");
-while ($row = mysqli_fetch_assoc($result)) {
-    $all_user_ids[] = $row['id'];
-}
-
-$result = mysqli_query($connection, "SELECT id FROM users, job_members WHERE users.id = job_members.user_id AND job_members.job_id = ".sanitize($_GET['job_id']));
-while ($row = mysqli_fetch_assoc($result)) {
-    $in_this_job_ids[] = $row['id'];
-}
-
-if (count($in_this_job_ids) > 0) {
-    $not_in_job = array_diff($all_user_ids, $in_this_job_ids);
-} else {
-    $not_in_job = $all_user_ids;
-}
-
-$not_used = "";
-if (count($not_in_job) > 0) {
-    foreach ($not_in_job as $not) {
-        $not_used .= $not.",";
+    
+    $result = mysqli_query($connection, "SELECT id FROM users");
+    while ($row = mysqli_fetch_assoc($result)) {
+        $all_user_ids[] = $row['id'];
     }
-    $not_used = substr($not_used,0,strlen($not_used)-1);
-}
-$result_x = mysqli_query($connection, "SELECT * FROM jobs WHERE id = ".sanitize($_GET['job_id']));
-$row_x = mysqli_fetch_assoc($result_x);
-
-?>
-<div class='panel_t'>
-<h2>Job Title: <span id="name"><?=$row_x['name']?></span></h2>
-Job Description: <span id="description"><?=$row_x['description']?></span>
-</div>
-<table>
-<tr>
-<td>
-<div class='panel_l'>
-<h2>In this job</h2>
-
-<ul class="swappers" id="list_1">
-<?
-$result = mysqli_query($connection, "SELECT * FROM users, job_members WHERE users.id = job_members.user_id AND job_members.job_id = ".sanitize($_GET['job_id']));
-while ($row = mysqli_fetch_assoc($result)) {
-    echo '<li id="user_'.$row['id'].'">'.$row['first_name'].' '.$row['last_name'].' ('.$row['username'].')</li>';
-}
-?>
-</ul>
-</div>
-</td>
-<td>
-<div class='panel_r'>
-<h2>Not in this job</h2>
-
-<ul class="swappers" id="list_2">
-<?
-if (count($not_in_job) > 0) {
-    $result = mysqli_query($connection, "SELECT * FROM users WHERE id IN (".$not_used.")") or die(mysqli_error($connection));
+    
+    $result = mysqli_query($connection, "SELECT id FROM users, job_members WHERE users.id = job_members.user_id AND job_members.job_id = ".sanitize($_GET['job_id']));
+    while ($row = mysqli_fetch_assoc($result)) {
+        $in_this_job_ids[] = $row['id'];
+    }
+    
+    if (count($in_this_job_ids) > 0) {
+        $not_in_job = array_diff($all_user_ids, $in_this_job_ids);
+    } else {
+        $not_in_job = $all_user_ids;
+    }
+    
+    $not_used = "";
+    if (count($not_in_job) > 0) {
+        foreach ($not_in_job as $not) {
+            $not_used .= $not.",";
+        }
+        $not_used = substr($not_used,0,strlen($not_used)-1);
+    }
+    $result_x = mysqli_query($connection, "SELECT * FROM jobs WHERE id = ".sanitize($_GET['job_id']));
+    $row_x = mysqli_fetch_assoc($result_x);
+    
+    ?>
+    <div class='panel_t'>
+    <h2>Job Title: <span id="name"><?=$row_x['name']?></span></h2>
+    Job Description: <span id="description"><?=$row_x['description']?></span>
+    </div>
+    <table>
+    <tr>
+    <td>
+    <div class='panel_l'>
+    <h2>In this job</h2>
+    
+    <ul class="swappers" id="list_1">
+    <?
+    $result = mysqli_query($connection, "SELECT * FROM users, job_members WHERE users.id = job_members.user_id AND job_members.job_id = ".sanitize($_GET['job_id']));
     while ($row = mysqli_fetch_assoc($result)) {
         echo '<li id="user_'.$row['id'].'">'.$row['first_name'].' '.$row['last_name'].' ('.$row['username'].')</li>';
     }
-}
-?>
-</ul>
-</div>
-
-</td>
-</tr>
-</table>
-
-<script>
-var counter = 0;
-var entries_to_ids=new Array();
-
-
-function delete_entry_from_database(item) {
-    //alert("Deleting item "+item+" from script <?=$_GET['edit']?> (id "+entries_to_ids[parseInt(item)]+")");
-    new Ajax.Request('jobs.php?delete_disposition='+entries_to_ids[parseInt(item)], {onSuccess: function(transport){
-    jQuery('#status_bar').text("Deleted Disposition");
-    jQuery('#status_bar').fadeIn(1000);    
-    jQuery('#status_bar').fadeOut(5000);    
-                     }});
-}
-
-function delete_entry(item) {
-    /* The item number is the number in the script starting from one - bearing in mind that there may be deleted
-     entries.  I.E. Item 1 may not be id 1.  If you had three entries and you delete id 1 and id 2 then item 1 would
-     be id 2 (id is zero based) */
-    Dialog.confirm('Are you sure you want to remove this disposition', {className:'alphacube', width:400, 
-                   okLabel: 'Yes', cancelLabel: 'No',
-                   onOk:function(win){
-                   jQuery("#entry"+item).remove();
-                   delete_entry_from_database(item);
-                   return true;
-                   }
-                   }
-                   );
-}
-
-function save_disposition(statement, divName){
-    new Ajax.Request('jobs.php?add_disposition=1',{parameters: {job_id: <?=$_GET['job_id']?>, text: statement}, onSuccess: function(transport){
-                     if (transport.responseText) {
-                     var response = transport.responseText;
-                     entries_to_ids[counter] = parseInt(response);
-                     jQuery('#status_bar').text("Saved Disposition");
-                     jQuery('#status_bar').fadeIn(1000);    
-                     jQuery('#status_bar').fadeOut(5000);
-
-                     }
-                     }
-                     });
-}
-
-function add_disposition(statement, divName){
-    counter++;
-    var newdiv = document.createElement('div');
-    newdiv.innerHTML = "<div class='disposition_entry' id='entry"+counter+"'><a href='#' onclick='delete_entry("+counter+");'><img src='images/delete.png' alt='Delete' width='16' height='16' align='right'></a>"+statement+"</div>";
-    document.getElementById(divName).appendChild(newdiv);
-}
-
-
-function add_new_disposition() {
-    Dialog.confirm('Disposition Text: <input type="text" id="disposition_text">', {className:'alphacube', width:400, 
-                   okLabel: 'Save', cancelLabel: 'cancel',
-                   onOk:function(win){
-                   save_disposition(jQuery('#disposition_text').val(), 'dynamicInput');
-                   add_disposition(jQuery('#disposition_text').val(), 'dynamicInput');
-                   return true;
-                   }
-                   }
-                   );
-}
-</script>
-
-<div class='panel_b'>
-<h2>Job Details</h2>
-Script: <select name="script" id="script" onchange="new Ajax.Request('jobs.php?save_script='+getUrlVars()['job_id'],{parameters: {script: jQuery('#script').val()}, onSuccess: function(transport){                     jQuery('#status_bar').text('Saved Script');jQuery('#status_bar').fadeIn(1000);jQuery('#status_bar').fadeOut(5000);if (transport.responseText) {var response = transport.responseText;alert(response);}}});">
-<option value="-1">Please select a script...</option>
-<?
-$result = mysqli_query($connection, "SELECT * FROM scripts");
-while ($row = mysqli_fetch_assoc($result)) {
+    ?>
+    </ul>
+    </div>
+    </td>
+    <td>
+    <div class='panel_r'>
+    <h2>Not in this job</h2>
     
-    echo '<option value="'.$row['id'].'" '.($row_x['script_id']==$row['id']?"SELECTED":"").'>'.$row['name'].'</option>';
-    //print_pre($row);
-}
-?></select>
-<br />
-<br />
+    <ul class="swappers" id="list_2">
+    <?
+    if (count($not_in_job) > 0) {
+        $result = mysqli_query($connection, "SELECT * FROM users WHERE id IN (".$not_used.")") or die(mysqli_error($connection));
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo '<li id="user_'.$row['id'].'">'.$row['first_name'].' '.$row['last_name'].' ('.$row['username'].')</li>';
+        }
+    }
+    ?>
+    </ul>
+    </div>
+    
+    </td>
+    </tr>
+    </table>
+    
+    <script>
+    var counter = 0;
+    var entries_to_ids=new Array();
+    
+    
+    function delete_entry_from_database(item) {
+        //alert("Deleting item "+item+" from script <?=$_GET['edit']?> (id "+entries_to_ids[parseInt(item)]+")");
+        new Ajax.Request('jobs.php?delete_disposition='+entries_to_ids[parseInt(item)], {onSuccess: function(transport){
+                         jQuery('#status_bar').text("Deleted Disposition");
+                         jQuery('#status_bar').fadeIn(1000);    
+                         jQuery('#status_bar').fadeOut(5000);    
+                         }});
+    }
+    
+    function delete_entry(item) {
+        /* The item number is the number in the script starting from one - bearing in mind that there may be deleted
+         entries.  I.E. Item 1 may not be id 1.  If you had three entries and you delete id 1 and id 2 then item 1 would
+         be id 2 (id is zero based) */
+        Dialog.confirm('Are you sure you want to remove this disposition', {className:'alphacube', width:400, 
+                       okLabel: 'Yes', cancelLabel: 'No',
+                       onOk:function(win){
+                       jQuery("#entry"+item).remove();
+                       delete_entry_from_database(item);
+                       return true;
+                       }
+                       }
+                       );
+    }
+    
+    function save_disposition(statement, divName){
+        new Ajax.Request('jobs.php?add_disposition=1',{parameters: {job_id: <?=$_GET['job_id']?>, text: statement}, onSuccess: function(transport){
+                         if (transport.responseText) {
+                         var response = transport.responseText;
+                         entries_to_ids[counter] = parseInt(response);
+                         jQuery('#status_bar').text("Saved Disposition");
+                         jQuery('#status_bar').fadeIn(1000);    
+                         jQuery('#status_bar').fadeOut(5000);
+                         
+                         }
+                         }
+                         });
+    }
+    
+    function add_disposition(statement, divName){
+        counter++;
+        var newdiv = document.createElement('div');
+        newdiv.innerHTML = "<div class='disposition_entry' id='entry"+counter+"'><a href='#' onclick='delete_entry("+counter+");'><img src='images/delete.png' alt='Delete' width='16' height='16' align='right'></a>"+statement+"</div>";
+        document.getElementById(divName).appendChild(newdiv);
+    }
+    
+    
+    function add_new_disposition() {
+        Dialog.confirm('Disposition Text: <input type="text" id="disposition_text">', {className:'alphacube', width:400, 
+                       okLabel: 'Save', cancelLabel: 'cancel',
+                       onOk:function(win){
+                       save_disposition(jQuery('#disposition_text').val(), 'dynamicInput');
+                       add_disposition(jQuery('#disposition_text').val(), 'dynamicInput');
+                       return true;
+                       }
+                       }
+                       );
+    }
+    </script>
+    
+    <div class='panel_b'>
+    <h2>Job Details</h2>
+Script: <select name="script" id="script" onchange="new Ajax.Request('jobs.php?save_script='+getUrlVars()['job_id'],{parameters: {script: jQuery('#script').val()}, onSuccess: function(transport){                     jQuery('#status_bar').text('Saved Script');jQuery('#status_bar').fadeIn(1000);jQuery('#status_bar').fadeOut(5000);if (transport.responseText) {var response = transport.responseText;alert(response);}}});">
+    <option value="-1">Please select a script...</option>
+    <?
+    $result = mysqli_query($connection, "SELECT * FROM scripts");
+    while ($row = mysqli_fetch_assoc($result)) {
+        
+        echo '<option value="'.$row['id'].'" '.($row_x['script_id']==$row['id']?"SELECTED":"").'>'.$row['name'].'</option>';
+        //print_pre($row);
+    }
+    ?></select>
+    <br />
+    <br />
 Dispositions: <a href="#" onclick="add_new_disposition();"><img src="images/add.png">&nbsp;Add Disposition</a>
-<br />
-<div id="dynamicInput"></div>
+    <br />
+    <div id="dynamicInput"></div>
+    <?
+    $result_entries = mysqli_query($connection, "SELECT * FROM job_dispositions WHERE job_id = ".sanitize($_GET['job_id']));
+    $x = 0;
+    if (mysqli_num_rows($result_entries) > 0) {
+        while ($row_entries = mysqli_fetch_assoc($result_entries)) {
+            $x++;
+            ?>
+            <script language="javascript">
+            entries_to_ids[<?=$x?>] = <?=$row_entries['id']?>;
+            add_disposition('<?=$row_entries['text']?>', 'dynamicInput');
+            </script>
+            <?
+        }
+    }
+    ?>
+    <br />
+    </div>
+    
+    <script>
+    jQuery( "#name" ).eip( "jobs.php?save_field=1" );
+    jQuery( "#description" ).eip( "jobs.php?save_field=1" );
+    </script>
+    
+    <?
+}
+$rounded[] = "div.box";
+require "header.php";
+?>
+<div class="box">
 <?
-$result_entries = mysqli_query($connection, "SELECT * FROM job_dispositions WHERE job_id = ".sanitize($_GET['job_id']));
-$x = 0;
-if (mysqli_num_rows($result_entries) > 0) {
-    while ($row_entries = mysqli_fetch_assoc($result_entries)) {
-        $x++;
+$result = mysqli_query($connection, "SELECT id, name, description FROM jobs") or die(mysqli_error($connection));;
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
         ?>
-        <script language="javascript">
-        entries_to_ids[<?=$x?>] = <?=$row_entries['id']?>;
-        add_disposition('<?=$row_entries['text']?>', 'dynamicInput');
-        </script>
+        <a href="jobs.php?job_id=<?=$row['id']?>"><img src="images/pencil.png" border="0">&nbsp;<?=$row['name']?></a><a href="jobs.php?delete=<?=$row['id']?>"><img src="images/delete.png" border="0"></a><br />
         <?
+        
     }
 }
 ?>
-<br />
 </div>
 
-<script>
-jQuery( "#name" ).eip( "jobs.php?save_field=1" );
-jQuery( "#description" ).eip( "jobs.php?save_field=1" );
-</script>
+<?
+require "footer.php";
+exit(0);
 
+?>
