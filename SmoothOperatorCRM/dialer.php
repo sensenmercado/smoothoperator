3235b0457c10af4e932f17062944eb8dc496e290
matt@venturevoip.com <matt@venturevoip.com@08a2b738-b151-11de-9d1a-45ab25924541>
2012-02-07 08:18:28 +0000

    Explain why we're doing what we're doing

--- SmoothOperatorCRM/dialer.php
+++ SmoothOperatorCRM/dialer.php
@@ -34,9 +34,13 @@ while ($row = mysql_fetch_assoc($result)) {
         /* Has a queue entry associated */
         $highest = -100;
         while ($row2 = mysql_fetch_assoc($result2)) {
-            if ($row['status'] >$highest && $row['status'] != 104 && $row['status'] != 4) {
-                $highest = $row['status'];
+            /* A status of 1 means about to start, 2 means about to stop.      */
+            /* Once processed it will add 100, so 101 means started, 102 means */
+            /* stopped.  4 and 104 are for changing agent numbers.             */
+            if ($row2['status'] >$highest && $row2['status'] != 104 && $row2['status'] != 4) {
+                $highest = $row2['status'];
             }
+            print_pre($row2);
         }
         $row['status'] = $highest;
     } else {
