ae75c0efa25131205078ca7618723054aaccfb3b
matt <matt@venturevoip.com>
2012-02-08 04:14:51 +0000

    Same order either way

--- SmoothOperatorCRM/dialer.php
+++ SmoothOperatorCRM/dialer.php
@@ -61,10 +61,10 @@ while ($row = mysql_fetch_assoc($result)) {
     } else {
         /* Does not have a queue entry associated */
         //echo "No Queue";
-        $row['status'] = 0;
         $row['progress'] = 0;
         $row['busy'] = 0;
         $row['total'] = 0;
+        $row['status'] = 0;
         $row['percentage_busy'] = 0.00;
     }
     
