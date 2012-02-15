ed662adfcea210b9d3a58aef89b25f6e5fe60b19
matt@venturevoip.com <matt@venturevoip.com@08a2b738-b151-11de-9d1a-45ab25924541>
2012-02-15 05:06:13 +0000

    Couple of changes - go to ajax updating and into test mode

--- SmoothOperatorCRM/dialer.php
+++ SmoothOperatorCRM/dialer.php
@@ -12,6 +12,156 @@
  start new campaigns running
  
  */
+
+if (isset($_GET['ajax'])) {
+    /* Start or continue a logged in session */
+    session_start();
+    $config_values = $_SESSION['config_values'];
+
+    /* Find out the current location */
+    $current_directory = dirname(__FILE__);
+    if (isset($override_directory)) {
+        $current_directory = $override_directory;
+    }
+    
+    /* Include VentureVoIP Functions */
+    require "functions/functions.php";
+    require "config/db_config.php";
+    $result = mysqli_query($connection, "SELECT * FROM jobs");
+    $job_ids = "";
+    if (mysqli_num_rows($result) == 0) {
+        /* No jobs */
+    } else {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $job_ids.= (100000+$row['id']).",";
+        }
+    }
+    /* cut off the last comma */
+    $job_ids = substr($job_ids,0,-1);
+
+    $link = mysql_connect($config_values['smoothtorque_db_host'], $config_values['smoothtorque_db_user'], $config_values['smoothtorque_db_pass']) or die(mysql_error());
+    $result = mysql_query("SELECT id, name FROM SineDialer.campaign where description = 'From SmoothOperator' and id in ($job_ids) ") or die(mysql_error());
+    ?>
+    <table class="sample2" width="100%">
+    <tbody>
+    <?
+    $header_printed = false;
+    while ($row = mysql_fetch_assoc($result)) {
+        $result2 = mysql_query("SELECT * FROM SineDialer.queue where campaignID = ".$row['id']);
+        if (mysql_num_rows($result2) > 0) {
+            /* Has a queue entry associated */
+            $highest = -100;
+            while ($row2 = mysql_fetch_assoc($result2)) {
+                /* A status of 1 means about to start, 2 means about to stop.      */
+                /* Once processed it will add 100, so 101 means started, 102 means */
+                /* stopped.  3 and 103 are for changing agent numbers.             */
+                if ($row2['status'] >$highest && $row2['status'] != 103 && $row2['status'] != 3) {
+                    $highest = $row2['status'];
+                    $row['progress'] = $row2['progress'];
+                    $row['busy'] = $row2['flags'];
+                    $row['total'] = $row2['maxcalls'];
+                }
+                //print_pre($row2);
+            }
+            $row['status'] = $highest;
+            if ($row['total'] > 0) {
+                $row['percentage_busy'] = round($row['busy']/$row['total']*100,2);
+            } else {
+                $row['percentage_busy'] = 0.00;
+            }
+        } else {
+            /* Does not have a queue entry associated */
+            //echo "No Queue";
+            $row['progress'] = 0;
+            $row['busy'] = 0;
+            $row['total'] = 0;
+            $row['status'] = 0;
+            $row['percentage_busy'] = 0.00;
+        }
+        
+        $result_x = mysqli_query($connection, "SELECT name FROM jobs WHERE id = ".($row['id']-100000));
+        $row_x =mysqli_fetch_assoc($result_x);
+        $row['name'] = $row_x['name'];
+        
+        if (!$header_printed) {
+            $header_printed = true;
+            echo "<tr>";
+            foreach ($row as $field=>$value) {
+                if ($field == "progress") {
+                    echo '<th><center>Dialed</center></th>';
+                } else if ($field == "id") {
+                } else {
+                    echo '<th><center>'.ucfirst(str_replace("_"," ",$field))."</center></th>";
+                }
+                
+            }
+            echo "</tr>";
+        }
+        echo "<tr>";
+        if ($row['status'] == 1 && $row['status'] == 101) {
+            // Campaign is running
+            $style=' style="background: #cfc"';
+        } else {
+            // Campaign is not running
+            $style=' style="background: #fff"';
+        }
+        //$row['percentage_busy']=30;
+        foreach ($row as $field=>$value) {
+            if ($field == "status") {
+                switch ($value) {
+                    case 1:
+                    case 101:
+                        // Running
+                        echo '<td '.$style.'>Running&nbsp;<a href="dialer.php?stop='.$row['id'].'"><img src="images/control_stop_blue.png" alt="Stop Campaign" border="0" valign="middle"></a></td>';
+                        break;
+                    case 103:
+                    case 104:
+                    case 3:
+                    case 4:
+                    case -1:
+                    case 102:
+                    case 0:
+                    case 2:
+                    default:
+                        // Not running
+                        echo '<td '.$style.'><center><a href="#" onclick="show_start('.$row['id'].',\''.$row['name'].'\');return false;">Not Running&nbsp;<img src="images/control_play_blue.png" alt="Stop Campaign" border="0" valign="middle"></a></center></td>';
+                        break;
+                }
+            } else if ($field == "id") {
+                
+            } else if ($field == "percentage_busy") {
+                echo "<td $style><center>";
+                ?>
+                <div id="perc_busy_<?=$row['id']?>" style="height: 10px; width: 150px"></div>
+                <script>
+                jQuery("#perc_busy_<?=$row['id']?>").progressbar({value: <?=$value?>});
+                </script>
+                <?
+                echo "</center></td>";
+                /*} else if ($field == "progress") {
+                 echo "<td $style><center>";
+                 ?>
+                 <div id="progress_<?=$row['id']?>" style="height: 10px; width: 150px"></div>
+                 <script>
+                 jQuery("#progress_<?=$row['id']?>").progressbar({value: <?=$value?>});
+                 </script>
+                 <?
+                 echo "</center></td>";*/
+            } else {
+                echo "<td $style><center>".$value."</center></td>";
+            }
+        }
+        echo "</tr>";
+        //print_pre($row);
+    }
+    ?>
+    </tbody>
+    </table>
+    <?
+    exit(0);
+}
+
+
 $rounded[] = "div.thin_700px_box";
 require "header.php";
 
@@ -95,13 +245,13 @@ if (isset($_GET['start_campaign'])) {
     $row_campaign = mysql_fetch_assoc($result_campaign);
     $clid = $row_campaign['clid'];
     $context = $row_campaign['context'];
-    
-    
+    $did = "nodid";
+    $mode = 1;
     
     // FOR TESTING: USE LOAD SIMULATION CONTEXT - CHANGE BACK AFTER
     $context = 0;
-    
-    
+    $did = "ls3";
+    $mode = 0;
     
     //print_pre($row_campaign);
     $result_customer = mysql_query("SELECT * FROM SineDialer.customer WHERE campaigngroupid = ".$row_campaign['groupid']) or die(mysql_error());
@@ -127,9 +277,11 @@ if (isset($_GET['start_campaign'])) {
     $trunkid = 506;
     $customerid = 1;
     //echo $queue_name;
+    $result = mysql_query("DELETE FROM SineDialer.queue WHERE campaignID = ".sanitize($_GET['start_campaign']));
+
     $sql = "INSERT INTO SineDialer.queue (`queuename`, `status`, `campaignID`, `details`, `flags`, `transferclid`, `starttime`, `endtime`, `startdate`, `enddate`, `did`, `clid`, `context`, `maxcalls`, `maxchans`, `maxretries`, `retrytime`, `waittime`, `timespent`, `progress`, `expectedRate`, `mode`, `astqueuename`, `trunk`, `accountcode`, `trunkid`, `customerID`, `maxcps`, `drive_min`, `drive_max`)
     VALUES
-    ('crm-autostart-".sanitize($_GET['start_campaign']-100000, false)."', 1, ".sanitize($_GET['start_campaign']-100000, false).", 'No details', 0, 'nocallerid', '00:00:00', '23:59:00', '2005-01-01', '2020-01-01', 'nodid', '$clid',$context, 30, $maxchans, 0, 0, 30, '0', '-1', 100, '1', '$queue_name', '$trunk', '$account', $trunkid, $customerid, $maxcps, '43.0', '61.0')";
+    ('crm-autostart-".sanitize($_GET['start_campaign']-100000, false)."', 1, ".sanitize($_GET['start_campaign'], false).", 'No details', 0, 'nocallerid', '00:00:00', '23:59:00', '2005-01-01', '2020-01-01', '$did', '$clid',$context, 30, $maxchans, 0, 0, 30, '0', '-1', 100, '$mode', '$queue_name', '$trunk', '$account', $trunkid, $customerid, $maxcps, '43.0', '61.0')";
     echo $sql;
     $result = mysql_query($sql);
     ?>
@@ -141,20 +293,7 @@ if (isset($_GET['start_campaign'])) {
     exit(0);
 }
 
-$result = mysqli_query($connection, "SELECT * FROM jobs");
-$job_ids = "";
-if (mysqli_num_rows($result) == 0) {
-    /* No jobs */
-} else {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $job_ids.= (100000+$row['id']).",";
-    }
-}
-/* cut off the last comma */
-$job_ids = substr($job_ids,0,-1);
 
-$link = mysql_connect($config_values['smoothtorque_db_host'], $config_values['smoothtorque_db_user'], $config_values['smoothtorque_db_pass']) or die(mysql_error());
-$result = mysql_query("SELECT id, name FROM SineDialer.campaign where description = 'From SmoothOperator' and id in ($job_ids) ") or die(mysql_error());
 ?>
 <script>
 var $the_dialog;
@@ -179,122 +318,19 @@ function show_start(campaign_id, title) {
 }
 </script>
 <div class="thin_700px_box">
-<table class="sample2" width="100%">
-<tbody>
-<?
-$header_printed = false;
-while ($row = mysql_fetch_assoc($result)) {
-    $result2 = mysql_query("SELECT * FROM SineDialer.queue where campaignID = ".$row['id']);
-    if (mysql_num_rows($result2) > 0) {
-        /* Has a queue entry associated */
-        $highest = -100;
-        while ($row2 = mysql_fetch_assoc($result2)) {
-            /* A status of 1 means about to start, 2 means about to stop.      */
-            /* Once processed it will add 100, so 101 means started, 102 means */
-            /* stopped.  3 and 103 are for changing agent numbers.             */
-            if ($row2['status'] >$highest && $row2['status'] != 103 && $row2['status'] != 3) {
-                $highest = $row2['status'];
-                $row['progress'] = $row2['progress'];
-                $row['busy'] = $row2['flags'];
-                $row['total'] = $row2['maxcalls'];
-            }
-            //print_pre($row2);
-        }
-        $row['status'] = $highest;
-        if ($row['total'] > 0) {
-            $row['percentage_busy'] = round($row['busy']/$row['total']*100,2);
-        } else {
-            $row['percentage_busy'] = 0.00;
-        }
-    } else {
-        /* Does not have a queue entry associated */
-        //echo "No Queue";
-        $row['progress'] = 0;
-        $row['busy'] = 0;
-        $row['total'] = 0;
-        $row['status'] = 0;
-        $row['percentage_busy'] = 0.00;
-    }
-    
-    $result_x = mysqli_query($connection, "SELECT name FROM jobs WHERE id = ".($row['id']-100000));
-    $row_x =mysqli_fetch_assoc($result_x);
-    $row['name'] = $row_x['name'];
-    
-    if (!$header_printed) {
-        $header_printed = true;
-        echo "<tr>";
-        foreach ($row as $field=>$value) {
-            if ($field == "progress") {
-                echo '<th><center>Dialed</center></th>';
-            } else if ($field == "id") {
-            } else {
-                echo '<th><center>'.ucfirst(str_replace("_"," ",$field))."</center></th>";
-            }
-            
-        }
-        echo "</tr>";
-    }
-    echo "<tr>";
-    if ($row['status'] == 1 && $row['status'] == 101) {
-        // Campaign is running
-        $style=' style="background: #cfc"';
-    } else {
-        // Campaign is not running
-        $style=' style="background: #fff"';
-    }
-    //$row['percentage_busy']=30;
-    foreach ($row as $field=>$value) {
-        if ($field == "status") {
-            switch ($value) {
-                case 1:
-                case 101:
-                    // Running
-                    echo '<td '.$style.'>Running&nbsp;<a href="dialer.php?stop='.$row['id'].'"><img src="images/control_stop_blue.png" alt="Stop Campaign" border="0" valign="middle"></a></td>';
-                    break;
-                case 103:
-                case 104:
-                case 3:
-                case 4:
-                case -1:
-                case 102:
-                case 0:
-                case 2:
-                default:
-                    // Not running
-                    echo '<td '.$style.'><center><a href="#" onclick="show_start('.$row['id'].',\''.$row['name'].'\');return false;">Not Running&nbsp;<img src="images/control_play_blue.png" alt="Stop Campaign" border="0" valign="middle"></a></center></td>';
-                    break;
-            }
-        } else if ($field == "id") {
-            
-        } else if ($field == "percentage_busy") {
-            echo "<td $style><center>";
-            ?>
-            <div id="perc_busy_<?=$row['id']?>" style="height: 10px; width: 150px"></div>
-            <script>
-            jQuery("#perc_busy_<?=$row['id']?>").progressbar({value: <?=$value?>});
-            </script>
-            <?
-            echo "</center></td>";
-            /*} else if ($field == "progress") {
-             echo "<td $style><center>";
-             ?>
-             <div id="progress_<?=$row['id']?>" style="height: 10px; width: 150px"></div>
-             <script>
-             jQuery("#progress_<?=$row['id']?>").progressbar({value: <?=$value?>});
-             </script>
-             <?
-             echo "</center></td>";*/
-        } else {
-            echo "<td $style><center>".$value."</center></td>";
-        }
-    }
-    echo "</tr>";
-    //print_pre($row);
-}
-?>
-</tbody>
-</table>
+<div id="ajax_content">
 </div>
+</div>
+<div id="ajax_status"><img src="images/progress.gif" valign="center"><br />Updating... <br /></div>
+<script>
+function update() {
+    jQuery("#ajax_status").fadeIn();
+    jQuery("#ajax_content").load("dialer.php?ajax=1", function() {jQuery("#ajax_status").fadeOut();});
+}
+update();
+setInterval("update()",15000);
+
+</script>
 <div id="start_campaign_dialog" style="display: none">
 <center>
 <br />
