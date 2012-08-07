40ce63fe509e45814c825bd5ed66744d56376eec
matt@venturevoip.com <matt@venturevoip.com@08a2b738-b151-11de-9d1a-45ab25924541>
2012-08-07 21:32:40 +0000

    Remove +1

--- SmoothOperatorCRM/check_calls.php
+++ SmoothOperatorCRM/check_calls.php
@@ -9,7 +9,7 @@ $sql = "SELECT * FROM phone_calls WHERE extension = ".sanitize($_GET['extension'
 $result = mysqli_query($connection, $sql);
 if ($result && mysqli_num_rows($result) > 0) {
     $row = mysqli_fetch_assoc($result);
-    echo $row['callerid'];
+    echo str_replace("+1","",$row['callerid']);
     mysqli_query($connection, "DELETE FROM phone_calls where id = ".$row['id']);
 }
 ?>
