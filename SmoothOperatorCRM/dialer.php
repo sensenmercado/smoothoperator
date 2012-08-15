2d8df3a17004e3eee8367ceab7e859c442299928
matt@venturevoip.com <matt@venturevoip.com@08a2b738-b151-11de-9d1a-45ab25924541>
2012-08-15 23:35:55 +0000

    Fix timezone error

--- SmoothOperatorCRM/dialer.php
+++ SmoothOperatorCRM/dialer.php
@@ -259,7 +259,7 @@ if (isset($_GET['start_campaign'])) {
         $result = mysql_query("select time_zones.start, time_zones.end, prefix from SineDialer.time_zones, timezone_prefixes where timezone_prefixes.timezone = time_zones.id");
         
         while ($row = mysql_fetch_assoc($result)) {
-            $sql = "UPDATE SineDialer.number set start_time = '".$row['start']."', end_time = '".$row['end']."', status='new' WHERE phonenumber like '".$row['prefix']."%' and status = 'new_nodial'";
+            $sql = "UPDATE SineDialer.number set start_time = '".$row['start']."', end_time = '".$row['end']."', status='new' WHERE phonenumber like '".$row['prefix']."%' and status = 'new'";
             $result2 = mysql_query($sql) or die(mysql_error());
             $i++;
             if ($i % 30) {
