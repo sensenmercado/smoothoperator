ac2bdfbfecb8b2db09e16d3f0279b442b703817e
matt@venturevoip.com <matt@venturevoip.com@08a2b738-b151-11de-9d1a-45ab25924541>
2012-08-16 20:47:04 +0000

    Add a test

--- SmoothOperatorCRM/outer_header.php
+++ SmoothOperatorCRM/outer_header.php
@@ -1,4 +1,13 @@
 <frameset cols="*, 150" border="1">
 <frame name="CRM" id="CRM" src="index.php" >
-<frame name="phone" id="phone" src="phono.php" >
+<?if (isset($_GET['debug'])) {
+    ?>
+    <frame name="phone" id="phone" src="phono.php?debug=1" >
+    <?
+} else {
+    ?>
+    <frame name="phone" id="phone" src="phono.php" >
+    <?
+}
+?>
 </frameset>
