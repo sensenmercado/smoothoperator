e9266fe425f34824b457eaebaa0f3c8db961512a
matt@venturevoip.com <matt@venturevoip.com@08a2b738-b151-11de-9d1a-45ab25924541>
2012-08-16 20:37:28 +0000

    Add ability to pause an agent

--- SmoothOperatorCRM/functions/asterisk_manager.php
+++ SmoothOperatorCRM/functions/asterisk_manager.php
@@ -53,4 +53,57 @@ if (!function_exists('asterisk_make_call')) {
         return $wrets;
     }
 }
+
+
+/*
+
+
+Action: QueuePause
+[ActionID:] <value>
+Interface: <value>
+Paused: <value>
+[Queue:] <value>
+[Reason:] <value>
+ */
+if (!function_exists('asterisk_agent_change_status')) {
+    function asterisk_agent_change_status($pause) {
+        global $config_values;
+        $timeout = 7500;
+        $socket = fsockopen($config_values['manager_host'],"5038", $errno, $errstr, $timeout);
+        if (!$socket) {
+            echo 'Socket fail<br>';
+            echo $errorno . '<br>';
+            echo $errstr . '<br>';
+            echo $timeout . '<br>';
+        } else {
+            fputs($socket, "Action: Login\r\n");
+            fputs($socket, "UserName: ".$config_values['manager_user']."\r\n");
+            fputs($socket, "Secret: ".$config_values['manager_pass']."\r\n");
+            fputs($socket, "Events: off\r\n\r\n");
+            do
+            {
+                $line = fgets($socket, 4096);
+                $wrets .= $line;
+                $info = stream_get_meta_data($socket);
+            } while ($line != "\r\n" && $info['timed_out'] == false );
+            
+            fputs($socket, "Action: QueuePause\r\n");
+            fputs($socket, "Interface: "."Agent/".$_SESSION['extension']."\r\n");
+            fputs($socket, "Paused: $pause\r\n");
+            do
+            {
+                $line = fgets($socket, 4096);
+                $wrets .= $line;
+                $info = stream_get_meta_data($socket);
+            } while ($line != "\r\n" && $info['timed_out'] == false );
+            fclose($socket);
+        }
+        return $wrets;
+    }
+}
+
+
+
+
+
 ?>
