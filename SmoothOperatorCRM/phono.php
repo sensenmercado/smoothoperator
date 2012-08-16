e2ffa395a91adfeb05f006cce1339ed0beb16267
matt@venturevoip.com <matt@venturevoip.com@08a2b738-b151-11de-9d1a-45ab25924541>
2012-08-16 19:04:49 +0000

    decrease bandwidth

--- SmoothOperatorCRM/phono.php
+++ SmoothOperatorCRM/phono.php
@@ -61,7 +61,7 @@ $(document).ready(function(){
                                       $("#call").attr("disabled", false).val("Login");
                                       },
                                       phone: {
-                                      wideband: true,
+                                      wideband: false,
                                       headset: true,
                                       
                                       onIncomingCall: function(event) {
