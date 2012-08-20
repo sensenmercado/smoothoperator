4e05bd56abe12660d74f8873a37ae69286e345ff
matt@venturevoip.com <matt@venturevoip.com@08a2b738-b151-11de-9d1a-45ab25924541>
2012-08-20 16:48:50 +0000

    Change charts

--- SmoothOperatorCRM/drawpie.php
+++ SmoothOperatorCRM/drawpie.php
@@ -10,28 +10,38 @@ exit(0);*/
 ?>
 <script type="text/javascript" src="https://www.google.com/jsapi"></script>
 <script type="text/javascript">
-google.load("visualization", "1", {packages:["corechart"]});
+google.load("visualization", "1", {packages: ["corechart"]});
 google.setOnLoadCallback(drawChart);
 function drawChart() {
-    var data = google.visualization.arrayToDataTable
-    ([
-     ['Date', 'Timeout', 'Answered', 'Busy', 'Congested', 'Answer Machine', 'Unknown', 'New'],
-     <?
-     $result = mysqli_query($connection, "SELECT (concat(report_date,' ',report_time)) as date, new, answered, busy, congested, amd, unknown, pressed1, hungup, timeout FROM campaign_stats WHERE campaign_id = 100003") or die(mysqli_error($connection));
-     $text = "";
-     while ($row = mysqli_fetch_assoc($result)) {
-        $text .= "['".$row['date']."',  ".$row['timeout'].",".($row['answered']+$row['pressed1']+$row['hungup']).",".$row['busy'].",".$row['congested'].",".$row['amd'].",".$row['unknown'].",".$row['new']."],";
-     }
-     echo substr($text,0,-1);
-     ?>
-     ]);
     
-    var options = {
+    var data = new google.visualization.DataTable();
+    data.addColumn('datetime', 'Date');
+    data.addColumn('number', 'Timeout');
+    data.addColumn('number', 'Answered');
+    data.addColumn('number', 'Busy');
+    data.addColumn('number', 'Congested');
+    data.addColumn('number', 'Answer Machine');
+    data.addColumn('number', 'Unknown');
+    data.addColumn('number', 'New');
+    
+    <?
+    $result = mysqli_query($connection, "SELECT unix_timestamp(concat(report_date,' ',report_time)) as date, new, answered, busy, congested, amd, unknown, pressed1, hungup, timeout FROM campaign_stats WHERE campaign_id = 100003") or die(mysqli_error($connection));
+    $text = "";
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "data.addRows([[new Date(".$row['date']."000), ".$row['timeout'].", ".($row['answered']+$row['pressed1']+$row['hungup']).", ".$row['busy'].", ".$row['congested'].", ".$row['amd'].", ".$row['unknown'].", ".$row['new']."]]);\n";
+//        echo "data.addRows(['".$row['date']."',".$row['timeout'].",".($row['answered']+$row['pressed1']+$row['hungup']).",".$row['busy'].",".$row['congested'].",".$row['amd'].",".$row['unknown'].",".$row['new']."]);\n";
+    }
+//    echo substr($text,0,-1);
+    ?>
+    
+    
+    
+       var options = {
     title: 'Number Status',
     areaOpacity: 1,
     colors: ['#444444','#88ff88','#0000ff','#ff0000','#ffff00','#888888','#eeffee'],
     isStacked: true,
-    hAxis: {title: 'Year',  titleTextStyle: {color: 'red'}}
+    hAxis: {title: 'Time',  titleTextStyle: {color: 'red'}}
     };
     
     var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
