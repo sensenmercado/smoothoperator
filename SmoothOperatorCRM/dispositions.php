2d1496362467e2c72b7b63fcbd2708d8fe5278e3
matt@venturevoip.com <matt@venturevoip.com@08a2b738-b151-11de-9d1a-45ab25924541>
2012-08-22 03:41:56 +0000

    Working with full downloads

--- SmoothOperatorCRM/dispositions.php
+++ SmoothOperatorCRM/dispositions.php
@@ -1,5 +1,16 @@
 <?
-require "header.php";
+function maybeEncodeCSVField($string) {
+    if(strpos($string, ',') !== false || strpos($string, '"') !== false || strpos($string, "\n") !== false) {
+        $string = '"' . str_replace('"', '""', $string) . '"';
+    }
+    return $string;
+}
+if (!isset($_GET['download'])) {
+    require "header.php";
+} else {
+    require "config/db_config.php";
+    require "functions/sanitize.php";
+}
 if (!isset($_GET['search'])) {
     ?>
     <script>
@@ -32,7 +43,7 @@ if (!isset($_GET['search'])) {
         echo 'Job: <br /><select name="job">';
         while ($row = mysqli_fetch_assoc($result)) {
             print_pre($row);
-            echo '<option value="'.id.'">'.$row['name'].'</option>';
+            echo '<option value="'.$row['id'].'">'.$row['name'].'</option>';
         }
         echo '</select><br /><br />';
     } else {
@@ -47,8 +58,79 @@ if (!isset($_GET['search'])) {
     <?
     box_end();
 } else {
-    print_pre($_POST);
-//    $result = mysqli
+    if (isset($_GET['job'])) {
+        $_POST['job'] = $_GET['job'];
+    }
+    if (isset($_GET['from_date'])) {
+        $_POST['from_date'] = $_GET['from_date'];
+    }
+    if (isset($_GET['to_date'])) {
+        $_POST['to_date'] = $_GET['to_date'];
+    }
+    if (!isset($_GET['download'])) {
+        ?>
+        <br />
+        <a href="dispositions.php?search=1&job=<?=$_POST['job']?>&from_date=<?=$_POST['from_date']?>&to_date=<?=$_POST['to_date']?>&download=1">Download Full List</a><br /><br />
+        <?
+    }
+    $sql = "SELECT customer_dispositions.*, customers.*, users.username, users.id FROM customer_dispositions, customers, users where date(contact_date_time) between ".sanitize($_POST['from_date'])." and ".sanitize($_POST['to_date'])." and customer_dispositions.job_id = ".sanitize($_POST['job'])." and customer_dispositions.customer_id = customers.id and customer_dispositions.user_id = users.id";
+    //    echo $sql;
+    $result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
+    $result_dispositions = mysqli_query($connection, "SELECT * FROM job_dispositions");
+    $disps = array();
+    while ($row_disp = mysqli_fetch_assoc($result_dispositions)) {
+        $disps[$row_disp['id']] = $row_disp['text'];
+    }
+    if (mysqli_num_rows($result) == 0) {
+        echo "No data found for that job/time period";
+    } else {
+        if (isset($_GET['download'])) {
+            header("Content-type: application/csv");
+            header("Content-Disposition: attachment; filename=Dispositions_Job_".$_POST['job']."_".$_POST['from_date']."-".$_POST['to_date'].".csv");
+            header("Pragma: no-cache");
+            header("Expires: 0");
+            $header_printed = false;
+            while ($row = mysqli_fetch_assoc($result)) {
+                if ($header_printed == false) {
+                    $header_printed = true;
+                    foreach ($row as $field=>$value) {
+                        echo $field.",";
+                    }
+                    echo "Unused\n";
+                }
+                $row['disposition'] = $disps[$row['disposition']];
+                foreach ($row as $field=>$value) {
+                    echo sanitize($value, false).",";
+                }
+                echo "''\n";
+            }
+        } else {
+            ?>
+            <div class="thin_90perc_box">
+            <table class="sample2" width="100%">
+            <?
+            echo '<tr>';
+            echo '<th>Contact Date/Time</th>';
+            echo '<th>Phone Number</th>';
+            echo '<th>Disposition</th>';
+            echo '<th>Username</th>';
+            echo '<th>Customer Name</th>';
+            echo '</tr>';
+            while ($row = mysqli_fetch_assoc($result)) {
+                //print_pre($row);
+                echo '<tr>';
+                echo '<td>'.$row['contact_date_time'].'</td>';
+                echo '<td>'.$row['cleaned_number'].'</td>';
+                echo '<td>'.$disps[$row['disposition']].'</td>';
+                echo '<td>'.$row['username'].'</td>';
+                echo '<td>'.$row['first_name']." ".$row['last_name'].'</td>';
+                echo '</tr>';
+            }
+            echo '</table></div>';
+        }
+    }
 }
-require "footer.php";
+if (!isset($_GET['download'])) {
+    require "footer.php";
+}
 ?>
