d2aa6c9b85afc866644a7693f82f5bac7cd10629
matt@venturevoip.com <matt@venturevoip.com@08a2b738-b151-11de-9d1a-45ab25924541>
2012-08-22 18:21:33 +0000

    Initial commit

---
+++ SmoothOperatorCRM/script_results.php
@@ -0,0 +1,81 @@
+<?
+require "header.php";
+if (!isset($_GET['search'])) {
+    ?>
+    <script>
+    jQuery(function() {
+           jQuery( "#from_date" ).datepicker({
+                                             dateFormat : 'yy-mm-dd'
+                                             });
+           jQuery( "#to_date" ).datepicker({
+                                           dateFormat : 'yy-mm-dd'
+                                           });
+           
+           });
+    </script>
+    <br />
+    <?
+    box_start();
+    ?>
+    <center>
+    <h3>Search for script_results</h3>
+        <form action = "script_results.php?search=1" method="post">
+        <p>From Date:
+        <input type="text" id="from_date" name="from_date" style="width: 200px">
+        <br />
+        To Date:
+        <input type="text" id="to_date"  name="to_date" style="width: 200px">
+        <br />
+        <?
+        $result = mysqli_query($connection, "SELECT * FROM SmoothOperator.jobs");
+    if (mysqli_num_rows($result) > 0) {
+        echo 'Job: <br /><select name="job">';
+        while ($row = mysqli_fetch_assoc($result)) {
+            print_pre($row);
+            echo '<option value="'.$row['id'].'">'.$row['name'].'</option>';
+        }
+        echo '</select><br /><br />';
+    } else {
+        ?>
+        echo "<h1>You need to create some jobs first!</h1>"
+        <?
+    }
+    ?>
+    <input type="submit" value="Display Call Dispositions">
+    </p>
+    </form>
+    <?
+    box_end();
+} else {
+    if (isset($_GET['job'])) {
+        $_POST['job'] = $_GET['job'];
+    }
+    if (isset($_GET['from_date'])) {
+        $_POST['from_date'] = $_GET['from_date'];
+    }
+    if (isset($_GET['to_date'])) {
+        $_POST['to_date'] = $_GET['to_date'];
+    }
+    if (!isset($_GET['download'])) {
+        ?>
+        <br />
+        <a href="script_results.php?search=1&job=<?=$_POST['job']?>&from_date=<?=$_POST['from_date']?>&to_date=<?=$_POST['to_date']?>&download=1">Download Full List</a><br /><br />
+        <?
+    }
+    $sql = "SELECT script_results.*, customers.*, users.username, users.id FROM script_results, customers, users where date(question_datetime) between ".sanitize($_POST['from_date'])." and ".sanitize($_POST['to_date'])." and script_results.job_id = ".sanitize($_POST['job'])." and script_results.customer_id = customers.id and script_results.user_id = users.id";
+    $result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
+    print_pre($result);
+    exit(0);
+    if (mysqli_num_rows($result) == 0) {
+        echo "There are no script results!";
+    } else {
+        while ($row = mysqli_fetch_assoc($result)) {
+            print_pre($row);
+        }
+    }
+}
+?>
+
+<?
+require "footer.php";
+?>
