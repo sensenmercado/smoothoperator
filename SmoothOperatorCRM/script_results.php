a5b148fcc7aed0fdecf948c363f6e756e7882fea
matt <matt@venturevoip.com>
2012-08-22 20:09:58 +0000

    Add script entries

--- SmoothOperatorCRM/script_results.php
+++ SmoothOperatorCRM/script_results.php
@@ -62,16 +62,37 @@ if (!isset($_GET['search'])) {
         <a href="script_results.php?search=1&job=<?=$_POST['job']?>&from_date=<?=$_POST['from_date']?>&to_date=<?=$_POST['to_date']?>&download=1">Download Full List</a><br /><br />
         <?
     }
-    $sql = "SELECT script_results.*, customers.*, users.username, users.id FROM script_results, customers, users where date(question_datetime) between ".sanitize($_POST['from_date'])." and ".sanitize($_POST['to_date'])." and script_results.job_id = ".sanitize($_POST['job'])." and script_results.customer_id = customers.id and script_results.user_id = users.id";
+    $sql = "SELECT script_results.*, customers.*, users.username, users.id, script_entries.statement, script_entries.type FROM script_results, customers, users, script_entries where date(question_datetime) between ".sanitize($_POST['from_date'])." and ".sanitize($_POST['to_date'])." and script_results.job_id = ".sanitize($_POST['job'])." and script_results.customer_id = customers.id and script_results.user_id = users.id and script_entries.script_id = script_results.script_id and script_entries.order = script_results.question_number";
     $result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
-    print_pre($result);
-    exit(0);
     if (mysqli_num_rows($result) == 0) {
         echo "There are no script results!";
     } else {
+        ?>
+        <div class="thin_90perc_box">
+        <table class="sample2" width="100%">
+        <?
+        echo '<tr>';
+        echo '<th>Contact Date/Time</th>';
+        echo '<th>Phone Number</th>';
+        echo '<th>Question</th>';
+        echo '<th>Answer</th>';
+        echo '<th>Username</th>';
+        echo '<th>Customer Name</th>';
+        echo '</tr>';
+
+        
         while ($row = mysqli_fetch_assoc($result)) {
-            print_pre($row);
+            //print_pre($row);
+            echo '<tr>';
+            echo '<td>'.$row['question_datetime'].'</td>';
+            echo '<td>'.$row['cleaned_number'].'</td>';
+            echo '<td>'.$row['statement'].'</td>';
+            echo '<td>'.$row['answer'].'</td>';
+            echo '<td>'.$row['username'].'</td>';
+            echo '<td>'.$row['first_name']." ".$row['last_name'].'</td>';
+            echo '</tr>';
         }
+        echo '</table></div>';
     }
 }
 ?>
