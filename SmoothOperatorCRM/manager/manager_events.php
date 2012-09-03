bb0847da22f6dfbd093a87149102c6346d68080c
matt <matt@venturevoip.com>
2012-09-03 16:23:33 +0000

    Fix bug

--- SmoothOperatorCRM/manager/manager_events.php
+++ SmoothOperatorCRM/manager/manager_events.php
@@ -159,7 +159,7 @@ function asterisk_link($chan_1, $chan_2, $clid_1, $clid_2, $type = "Unknown") {
         echo "Leg 2 was local, returning\n";
         return;
     }
-    echo "Local not detected - continuing\n"
+    echo "Local not detected - continuing\n";
     if (substr($chan_1,0,5) == "Agent") {
         $sql = "INSERT INTO SmoothOperator.phone_calls (callerid, extension) VALUES ('".$clid_2."','".substr($chan_1,6)."')";
         echo "Running $sql";
