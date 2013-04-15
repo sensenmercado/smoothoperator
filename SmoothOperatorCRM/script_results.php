0c06abbbbfcbb01bf43fef7d79af1e6bedb6382a
matt@venturevoip.com <matt@venturevoip.com@08a2b738-b151-11de-9d1a-45ab25924541>
2013-04-15 18:53:54 +0000

    Disps

--- SmoothOperatorCRM/script_results.php
+++ SmoothOperatorCRM/script_results.php
@@ -1,9 +1,18 @@
 <?
-if (!isset($_GET['download'])) {
+if ((!isset($_GET['download'])) && (!isset($_GET['get_dispositions']))) {
     require "header.php";
 } else {
     require "config/db_config.php";
     require "functions/sanitize.php";
+}
+if (isset($_GET['get_dispositions'])) {
+    //    $array = array(1,2,3,4,5,6);
+    $result = mysqli_query($connection, "SELECT id, text FROM job_dispositions where job_id = ".sanitize($_GET['get_dispositions']));
+    while ($row = mysqli_fetch_assoc($result)) {
+        $array[$row['id']] = ucwords($row['text']);
+    }
+    echo json_encode($array);
+    exit(0);
 }
 if (!isset($_GET['search'])) {
     ?>
@@ -34,10 +43,11 @@ if (!isset($_GET['search'])) {
         <?
         $result = mysqli_query($connection, "SELECT * FROM SmoothOperator.jobs");
     if (mysqli_num_rows($result) > 0) {
-        echo 'Job: <br /><select name="job">';
+        echo 'Job: <br /><select name="job" onchange="update_dispositions(jQuery(this).val())">';
+        echo '<option value="-1" >-- Please Select a Job --</option>';
         while ($row = mysqli_fetch_assoc($result)) {
             print_pre($row);
-            echo '<option value="'.$row['id'].'">'.$row['name'].'</option>';
+            echo '<option value="'.$row['id'].'" >'.$row['name'].'</option>';
         }
         echo '</select><br /><br />';
     } else {
@@ -46,6 +56,23 @@ if (!isset($_GET['search'])) {
         <?
     }
     ?>
+    <div id="content1">
+    </div>
+    <br />
+    <script>
+    function update_dispositions(id) {
+        jQuery("#content1").html("");
+        jQuery.getJSON('script_results.php?get_dispositions='+id, function(data) {
+                       var items = [];
+                       jQuery.each(data, function(key, val) {
+//                                   <input type="checkbox" name="formDoor[]" value="D" />Drake Commons
+                                   jQuery("#content1").append('<input type="checkbox" name="dispositions[]" value="' + key + '"> ' + val +'<br />');
+                                   });
+                       });
+    }
+    </script>
+    
+    
     <input type="submit" value="Display Call Dispositions">
     </p>
     </form>
@@ -61,16 +88,28 @@ if (!isset($_GET['search'])) {
     if (isset($_GET['to_date'])) {
         $_POST['to_date'] = $_GET['to_date'];
     }
+    if (isset($_GET['dispositions[]'])) {
+        $_POST['dispositions[]'] = $_GET['dispositions[]'];
+    }
+    $dispositions = "";
+    foreach ($_POST['dispositions'] as $disposition) {
+        $dispositions.=$disposition.",";
+    }
+    $dispositions = substr($dispositions,0,-1);
+    //echo $dispositions;
+    $stuff['dispositions']= $_POST['dispositions'];
+    $disps= http_build_query($stuff);
+    //exit(0);
     if (!isset($_GET['download'])) {
         ?>
         <br />
-        <a href="script_results.php?search=1&job=<?=$_POST['job']?>&from_date=<?=$_POST['from_date']?>&to_date=<?=$_POST['to_date']?>&download=1">Download Full List</a><br /><br />
+        <a href="script_results.php?search=1&job=<?=$_POST['job']?>&from_date=<?=$_POST['from_date']?>&to_date=<?=$_POST['to_date']?>&download=1&<?=$disps?>">Download Full List</a><br /><br />
         <?
     }
     if (isset($_GET['show_lead'])) {
         $sql = "SELECT script_entries.statement, script_results.*, customers.*, users.username, users.id,  customers.id as customer_id, script_entries.type FROM script_entries, script_results, customers, users where date(question_datetime) between ".sanitize($_POST['from_date'])." and ".sanitize($_POST['to_date'])." and script_results.job_id = ".sanitize($_POST['job'])." and script_results.customer_id = customers.id and script_results.user_id = users.id and script_entries.script_id = script_results.script_id and script_entries.order = script_results.question_number and customers.id = ".sanitize($_GET['customer_id']);
     } else {
-        $sql = "SELECT script_entries.statement, script_results.*, job_dispositions.text as disposition_text, customers.*, users.username, users.id,  customers.id as customer_id, script_entries.type FROM script_entries, script_results, customers, users, customer_dispositions, job_dispositions where date(question_datetime) between ".sanitize($_POST['from_date'])." and ".sanitize($_POST['to_date'])." and script_results.job_id = ".sanitize($_POST['job'])." and script_results.customer_id = customers.id and script_results.user_id = users.id and script_entries.script_id = script_results.script_id and script_entries.order = script_results.question_number and customer_dispositions.`customer_id`=customers.id and job_dispositions.id = disposition";
+        $sql = "SELECT script_entries.statement, script_results.*, job_dispositions.text as disposition_text, customers.*, users.username, users.id,  customers.id as customer_id, script_entries.type FROM script_entries, script_results, customers, users, customer_dispositions, job_dispositions where date(question_datetime) between ".sanitize($_POST['from_date'])." and ".sanitize($_POST['to_date'])." and script_results.job_id = ".sanitize($_POST['job'])." and script_results.customer_id = customers.id and script_results.user_id = users.id and script_entries.script_id = script_results.script_id and script_entries.order = script_results.question_number and customer_dispositions.`customer_id`=customers.id and job_dispositions.id = disposition and job_dispositions.id in (".$dispositions.")";
     }
     $result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
     //echo $sql;
